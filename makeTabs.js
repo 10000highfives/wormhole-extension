ab23dc8c967b52268d0c7e7541594b6e678c8399
10000highfives <jared.a.fowler@gmail.com>
2016-02-03 23:42:27 -0800

    cleans

--- makeTabs.js
+++ makeTabs.js
@@ -1,11 +1,7 @@
 //make tabs
-makeTabsFromArray(getHeadings());
-
-
-
 function makeTabsFromArray(arrayOfSearchTermsStrings) {
-  console.log('array at i ', arrayOfSearchTermsStrings[i]);
   for (var i = 0; i < arrayOfSearchTermsStrings.length; i++) {
-    chrome.tabs.create({ url: "http://www.google.com/#q=" + term, windowId: i + 1 });
+		console.log('array at i ', arrayOfSearchTermsStrings[i]);
+    chrome.tabs.create({ url: "http://www.google.com/#q=" + arrayOfSearchTermsStrings[i], windowId: i + 1 });
   }
 }
